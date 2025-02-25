4991ca4bbb2c03e564f4a296ecd265e63aaa3497
DGBIZT <georgiy.gorlov@gmail.com>
2025-02-25 23:04:48 +0300

    add function get_date

--- src/widget.py
+++ src/widget.py
@@ -1,6 +1,9 @@
 from .masks import get_mask_account, get_mask_card_number
 
 
+"""Функция которая обрабатывает информацию как о картах, так и о счетах"""
+
+
 def mask_account_card(card: str) -> str:
     split_card = card.split(" ")
     if len(split_card[-1]) == 16:
@@ -9,3 +12,13 @@ def mask_account_card(card: str) -> str:
         mask_ac_card = f"{" ".join(split_card[:-1])} {get_mask_account(split_card[-1])}"
 
     return mask_ac_card
+
+
+"""Функция которая принимает на вход строку с датой в формате "2024-03-11T02:26:18.671407"
+ и возвращает строку с датой в формате "ДД.ММ.ГГГГ" """
+
+
+def get_date(date: str) -> str:
+    data_new = f"{date[8:10]}.{date[5:7]}.{date[:4]}"
+
+    return data_new
