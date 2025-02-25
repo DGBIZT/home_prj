d6397dece2214ac3b2b7f73654bca244c7937537
DGBIZT <georgiy.gorlov@gmail.com>
2025-02-25 23:46:34 +0300

    Добавил в main.py проверку функций get_date и mask_account_card

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from src.masks import get_mask_account, get_mask_card_number
+from src.widget import get_date, mask_account_card
 
 # Маскировка номера банковской карты
 print(get_mask_card_number("7000792289606361"))
 
 # Маскировка номера банковского счета
 print(get_mask_account("73654108430135874305"))
+
+# Обрабатка информации как о картах, так и о счетах
+print(mask_account_card("Счет 73654108430135874305"))
+
+# Обработка даты
+print(get_date("2024-03-11T02:26:18.671407"))
