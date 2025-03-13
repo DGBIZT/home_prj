51822db39c292625d253711e9e9076bbb3bbf5ca
DGBIZT <georgiy.gorlov@gmail.com>
2025-03-13 21:13:30 +0300

    доработал функцию get_date и создал проверочные тесты test_get_date и добавил парочку тестов в test_mask_account_card

--- tests/test_masks_card.py
+++ tests/test_masks_card.py
@@ -28,3 +28,18 @@ def test_mask_account_card():
     assert mask_account_card("Maestro 7000792289606361") == "Maestro 7000 79** **** 6361"
     assert mask_account_card("Счет 73654108430135874305") == "Счет **4305"
     assert mask_account_card("MasterCard 7158300734726758") == "MasterCard 7158 30** **** 6758"
+    with pytest.raises(AttributeError):
+        mask_account_card(125)
+    with pytest.raises(AttributeError):
+        mask_account_card(None)
+
+def test_get_date():
+    assert get_date("2024-03-11T02:26:18.671407") == "11.03.2024"
+    with pytest.raises(UnboundLocalError):
+        get_date("11-03-2020T02:26:18.671407")
+    with pytest.raises(UnboundLocalError):
+        get_date("024-03-11T02:26:18.671407")
+
+
+
+
