cfa3f5c327f1c36c1952e54d93ddd0a499d747f6
George Gorlov <Georgiy.Gorlov@gmail.com>
2025-03-13 13:35:15 +0300

    В фушкцию get_mask_card_number добавил исключение ValueError и в модуле test_masks_card добавил проверку на нестандартные длины номера и отсутствия номера

--- src/masks.py
+++ src/masks.py
@@ -1,10 +1,12 @@
 def get_mask_card_number(card_number: str) -> str:
     """Функция маскировки номера банковской карты"""
-
-    if len(card_number) == 16:
+    if len(card_number) != 16:
+        raise ValueError("Номер карты должен содержать 16 цифр")
+    else:
         mask_card_number = f"{card_number[0:4]} {card_number[4:6]}** **** {card_number[-4:]}"
 
     return mask_card_number
+
 
 
 def get_mask_account(account_number: str) -> str:
@@ -12,5 +14,6 @@ def get_mask_account(account_number: str) -> str:
 
     if len(account_number) == 20:
         mask_account_number = f"**{account_number[-4:]}"
-
-    return mask_account_number
+        return mask_account_number
+    else:
+        raise ValueError("Номер карты должен содержать 20 цифр")
