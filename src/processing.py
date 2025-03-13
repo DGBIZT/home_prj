c8b5eca28cad1b69b9a165384e572d737e48a55a
DGBIZT <georgiy.gorlov@gmail.com>
2025-03-13 23:29:49 +0300

    оработал функцию sort_by_date и добавли пару тестов в директории tests

--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,4 @@
+from src.widget import get_date
 def filter_by_state(list_of_dicts: list[dict[str, int | str]], state: str = "EXECUTED") -> list:
     """Функция возвращает новый список словарей, содержащий только те словари, у которых ключ 'state'
     соответствует указаному значению"""
@@ -11,9 +12,14 @@ def filter_by_state(list_of_dicts: list[dict[str, int | str]], state: str = "EXE
 
     return new_list
 
-def sort_by_date(list_of_dicts: list[dict[str, int | str]]) -> list:
+def sort_by_date(list_of_dicts: list[dict[str, int | str]], state: bool = None) -> list:
     """Функция, которая принимает список словарей и возвращает новый список, отсортированный по дате"""
 
-    new_list_sort_data = sorted(list_of_dicts, key=lambda new_list: new_list["date"], reverse=True)
+    for item in list_of_dicts:
+       if get_date(item["date"]):
+           if state is None:
+               list_of_dicts = sorted(list_of_dicts, key=lambda new_list: new_list["date"], reverse=True)
+           else:
+               list_of_dicts = sorted(list_of_dicts, key=lambda new_list: new_list["date"])
 
-    return new_list_sort_data
+    return list_of_dicts
--- tests/test_masks_card.py
+++ tests/test_masks_card.py
@@ -14,6 +14,7 @@ def test_get_mask_card_number():
     with pytest.raises(ValueError):
         get_mask_card_number(" ")
 
+
 def test_get_mask_account():
     assert get_mask_account("73654108430135874305") == "**4305"
 
@@ -34,6 +35,7 @@ def test_mask_account_card():
     with pytest.raises(AttributeError):
         mask_account_card(None)
 
+
 def test_get_date():
     assert get_date("2024-03-11T02:26:18.671407") == "11.03.2024"
     with pytest.raises(UnboundLocalError):
@@ -43,27 +45,53 @@ def test_get_date():
     with pytest.raises(UnboundLocalError):
         get_date("")
 
+
 def test_filter_by_state():
     assert filter_by_state(
-    [
-        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-    ]
-)
+        [
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        ]
+    )
     with pytest.raises(NameError):
         filter_by_state([])
     assert filter_by_state(
         [
-        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-    ], "CANCELED"
-)
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        ],
+        "CANCELED",
+    )
 
 
-
+def test_sort_by_date():
+    assert sort_by_date(
+        [
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        ]
+    )
+    assert sort_by_date(
+        [
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        ]
+    )
+    assert sort_by_date(
+        [
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        ], True
+    )
