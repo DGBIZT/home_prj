83e390b183887fad7c8b047562a23ea3be7a9ab8
DGBIZT <georgiy.gorlov@gmail.com>
2025-03-14 21:35:43 +0300

    доработал функцию sort_by_date в модуле processing и в дериктории tests добавил тесты

--- src/processing.py
+++ src/processing.py
@@ -1,25 +1,35 @@
 from src.widget import get_date
+
+
 def filter_by_state(list_of_dicts: list[dict[str, int | str]], state: str = "EXECUTED") -> list:
     """Функция возвращает новый список словарей, содержащий только те словари, у которых ключ 'state'
-    соответствует указаному значению"""
+    соответствует указанному значению"""
 
     new_list = []
     for item in list_of_dicts:
         if item["state"] == state:
             new_list.append(item)
     if new_list == []:
-            raise NameError("Заданный ключ state отсутствует")
+        raise NameError("Заданный ключ state отсутствует")
 
     return new_list
 
-def sort_by_date(list_of_dicts: list[dict[str, int | str]], state: bool = None) -> list:
-    """Функция, которая принимает список словарей и возвращает новый список, отсортированный по дате"""
 
+def sort_by_date(list_of_dicts: list[dict[str, int | str]], state: bool=False) -> list:
+    """Функция, которая принимает список словарей и возвращает новый список, отсортированный по дате"""
+    if list_of_dicts == []:
+        raise UnboundLocalError("Ваш список пуст, заполните список что бы провести сортировку")
     for item in list_of_dicts:
-       if get_date(item["date"]):
-           if state is None:
-               list_of_dicts = sorted(list_of_dicts, key=lambda new_list: new_list["date"], reverse=True)
-           else:
-               list_of_dicts = sorted(list_of_dicts, key=lambda new_list: new_list["date"])
+        if isinstance(item["date"], str):
+            if get_date(item["date"]):
+                split_date = item["date"].split("-")
+                if len(split_date[0]) != 4:
+                    raise UnboundLocalError("Введите значение с датой в формате 2024-03-11T02:26:18.671407")
+                if state == False:
+                    list_of_dicts = sorted(list_of_dicts, key=lambda new_list: new_list["date"], reverse=True)
+                else:
+                    list_of_dicts = sorted(list_of_dicts, key=lambda new_list: new_list["date"])
+        else:
+            raise SyntaxError("Дата должна быть строковым типом данных")
 
     return list_of_dicts
--- tests/test_masks_card.py
+++ tests/test_masks_card.py
@@ -95,3 +95,15 @@ def test_sort_by_date():
             {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
         ], True
     )
+    with pytest.raises(UnboundLocalError):
+        sort_by_date([
+            {"id": 41428829, "state": "EXECUTED", "date": "19-07-03T18:35:29.512364"},
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"}
+        ]
+        )
+    with pytest.raises(UnboundLocalError):
+        sort_by_date([])
+    with pytest.raises(SyntaxError):
+        sort_by_date([{"id": 41428829, "state": "EXECUTED", "date": 20190703183529512364}])
+    with pytest.raises(UnboundLocalError):
+        sort_by_date([{"id": 41428829, "state": "EXECUTED", "date": "19-07-03T18:35:29.512364"}])
