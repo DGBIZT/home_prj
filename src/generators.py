421e817626872925b6fd6db772f17c1cac403acd
DGBIZT <georgiy.gorlov@gmail.com>
2025-03-20 21:59:02 +0300

    Дописал функцию filter_by_currency, добавил исключения и исправил функцию

--- src/generators.py
+++ src/generators.py
@@ -1,16 +1,32 @@
-from typing import Iterator
-from operator import itemgetter
+from typing import Any, Generator, Iterator
 
-def filter_by_currency(transaction: list[dict[str, int | str]], forex: str) -> Iterator[dict[str, int | str]]:
-     currency_code = list((x for x in transaction if x["operationAmount"]["currency"]["code"] == forex))
-     yield currency_code
+
+def filter_by_currency(transaction: list[dict[str, int | str]], forex: str) -> Iterator[list[dict[str, int | str]]]:
+    """Функция принимает на вход список словарей, представляющих транзакции и возвращает итератор,
+    который поочередно выдает транзакции"""
+
+    if transaction == []:
+        raise UnboundLocalError("Ваш список пуст, заполните его")
+    elif forex == "":
+        raise TypeError("Введите валюту для получения информации")
+    for _ in range(len(transactions)):
+        total_number = 0
+        for item in transactions:
+            if item["operationAmount"]["currency"]["code"] != forex:
+                total_number += 1
+    if total_number == len(transactions):
+        raise ValueError("По данной валюте нет транзакций!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    else:
+        currency_code = list((x for x in transaction if x["operationAmount"]["currency"]["code"] == forex))
+        for item in currency_code:
+            yield item
 
 
 transactions = [
     {
         "id": 970157810,
         "date": "2018-06-08T10:3:58.027767",
-        "operationAmount": {"amount": "150", "currency": {"code": "USD"}},
+        "operationAmount": {"amount": "150", "currency": {"code": "RUB"}},
         "description": "Перевод организации",
         "from": "Счет - 63475662387234505765",
         "to": "Счет 8175128657841941437",
@@ -26,21 +42,24 @@ transactions = [
     {
         "id": 999888555,
         "date": "2025-03-19T10:3:58.027767",
-        "operationAmount": {"amount": "250", "currency": {"code": "USD"}},
+        "operationAmount": {"amount": "250", "currency": {"code": "RUB"}},
         "description": "Перевод организации STMU",
         "from": "Счет - 63475662387234505765",
         "to": "Счет 8175128657841941437",
     },
 ]
-usd_transactions = filter_by_currency(transactions, "USD")
-for _ in range(1):
-    print(*(next(usd_transactions)))
+usd_transactions_list = list(filter_by_currency(transactions, "RUB"))
+number_of_iterations = len(usd_transactions_list)
+for i in range(number_of_iterations):
+    print(usd_transactions_list[i])
 
-def transaction_descriptions(transaction: list[dict[str, int | str]]) -> str:
+
+def transaction_descriptions(transaction: list[dict[str, int | str]]) -> Generator[int | str, Any, None]:
+    """Функция принимает список словарей с транзакциями и возвращает описание каждой операции по очереди"""
     for num in transaction:
         yield num["description"]
 
-descriptions = transaction_descriptions(transactions)
 
-for i in range(len(transactions)):
+descriptions = transaction_descriptions(usd_transactions_list)
+for i in range(number_of_iterations):
     print(next(descriptions))
