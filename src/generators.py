8b503431ad329449f6e7d4e6d1508a08a8458415
DGBIZT <georgiy.gorlov@gmail.com>
2025-03-21 22:52:14 +0300

    Написал тесты для всех функций и генераторов для модуля generators.py

--- src/generators.py
+++ src/generators.py
@@ -6,16 +6,17 @@ def filter_by_currency(transaction: list[dict[str, int | str]], forex: str) -> I
     который поочередно выдает транзакции"""
 
     if transaction == []:
-        raise UnboundLocalError("Ваш список пуст, заполните его")
+        raise TypeError("Ваш список пуст, заполните его")
     elif forex == "":
         raise TypeError("Введите валюту для получения информации")
+
     for _ in range(len(transactions)):
         total_number = 0
         for item in transactions:
             if item["operationAmount"]["currency"]["code"] != forex:
                 total_number += 1
     if total_number == len(transactions):
-        raise ValueError("По данной валюте нет транзакций!")
+        raise TypeError("По данной валюте нет транзакций!")
     else:
         currency_code = list((x for x in transaction if x["operationAmount"]["currency"]["code"] == forex))
         for item in currency_code:
@@ -25,13 +26,11 @@ def filter_by_currency(transaction: list[dict[str, int | str]], forex: str) -> I
 def transaction_descriptions(transaction: list[dict[str, int | str]]) -> Generator[int | str, Any, None]:
     """Функция принимает список словарей с транзакциями и возвращает описание каждой операции по очереди"""
 
-    if transaction == []:
-        raise UnboundLocalError("Ваш список пуст, заполните его")
     for num in transaction:
         yield num["description"]
 
 
-def card_number_generator(start: int, end: int) -> str:
+def card_number_generator(start: int, end: int):
     """Генератор, который выдает номера банковских карт в формате XXXX XXXX XXXX XXXX, где X - цифра номера карты"""
 
     for num in range(start, end + 1):
--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.generators import filter_by_currency, transaction_descriptions, card_number_generator
+from src.generators import card_number_generator, filter_by_currency, transaction_descriptions
 
 
 def test_filter_by_currency():
@@ -14,6 +14,16 @@ def test_filter_by_currency():
             "to": "Счет 1743370781324891402",
         },
         {
+            "id": 999888555,
+            "date": "2025-03-19T10:3:58.027767",
+            "operationAmount": {"amount": "250", "currency": {"code": "USD"}},
+            "description": "Перевод организации STMU",
+            "from": "Счет - 63475662387234505765",
+            "to": "Счет 8175128657841941437",
+        },
+    ]
+    transactions = [
+        {
             "id": 129309161,
             "date": "2018-09-26T00:46:36.256087",
             "operationAmount": {"amount": "17250", "currency": {"code": "USD"}},
@@ -21,8 +31,36 @@ def test_filter_by_currency():
             "from": "Visa Classic 1313132313442324",
             "to": "Счет 1743370781324891402",
         },
+        {
+            "id": 999888555,
+            "date": "2025-03-19T10:3:58.027767",
+            "operationAmount": {"amount": "250", "currency": {"code": "USD"}},
+            "description": "Перевод организации STMU",
+            "from": "Счет - 63475662387234505765",
+            "to": "Счет 8175128657841941437",
+        },
     ]
-    result = list((x for x in expected_result if x["operationAmount"]["currency"]["code"] == "USD"))
+
+    result = list(
+        (x for x in filter_by_currency(transactions, "USD") if x["operationAmount"]["currency"]["code"] == "USD")
+    )
     for item in result:
-        yield item
-    assert filter_by_currency == result
+        assert item in expected_result
+    with pytest.raises(TypeError):
+        filter_by_currency("")
+    with pytest.raises(TypeError):
+        filter_by_currency([])
+    with pytest.raises(TypeError):
+        filter_by_currency(0)
+
+
+def test_transaction_descriptions(transaction_descriptions_mylist):
+    result = next(transaction_descriptions(transaction_descriptions_mylist))
+    assert result == "Перевод с карты на счет"
+
+
+def test_card_number_generator():
+    generator = card_number_generator(1, 3)
+    assert next(generator) == "0000 0000 0000 0001"
+    assert next(generator) == "0000 0000 0000 0002"
+    assert next(generator) == "0000 0000 0000 0003"
