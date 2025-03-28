06c178a9fc91b189091edf1eb759a38374983c08
George Gorlov <Georgiy.Gorlov@gmail.com>
2025-03-28 15:43:18 +0300

    Создал test_decorators и написал один тест

--- src/decorators.py
+++ src/decorators.py
@@ -1,39 +1,65 @@
+import os
+from datetime import datetime
 from functools import wraps
-import os, time
+
 
 def log(filename=""):
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
             try:
-                time_start = time.time()
-                result = func(*args , **kwargs)
-                end_time = time.time()
-                print(f"{func.__name__}: Результат {result}")
+                time_start = datetime.now()
+                formatted_time = time_start.strftime("%Y-%m-%d %H:%M:%S")
+                result = func(*args, **kwargs)
+                end_time = datetime.now()
+                formatted_time_end = end_time.strftime("%Y-%m-%d %H:%M:%S")
+
+                # print(f"{func.__name__}: Результат {result}")
                 if filename:
-                    with open(os.path.abspath(f"{filename}"), 'a', encoding="utf-8") as f:
-                        f.write(f"{time_start} \n {func.__name__} ок \n {end_time} ")
+                    with open(os.path.abspath(f"{filename}"), "a", encoding="utf-8") as f:
+                        f.write(
+                            f"\n[{formatted_time}] {func.__name__} started with inputs: {args}, {kwargs}\n"
+                            f"[{formatted_time_end}] {func.__name__} finished successfully with result: {result}"
+                        )
                 else:
-                    print(f"{func.__name__} ок")
-                end_time = time.time()
+                    print(
+                        f"\n[{formatted_time}] {func.__name__} started with inputs: {args}, {kwargs}\n"
+                        f"[{formatted_time_end}] {func.__name__} finished successfully with result: {result}"
+                    )
                 return result
+
             except TypeError as e:
+                time_start_error = datetime.now()
+                formatted_time_error_start = time_start_error.strftime("%Y-%m-%d %H:%M:%S")
+                time_end_error = datetime.now()
+                formatted_time_error_end = time_end_error.strftime("%Y-%m-%d %H:%M:%S")
                 if filename:
                     print(f"{func.__name__} as {e}")
-                    with open(os.path.abspath(f"{filename}"), 'a', encoding="utf-8") as f:
-                        f.write(f"{func.__name__} error: {type(e).__name__}. Inputs: {args}, {kwargs}  \n")
+                    with open(os.path.abspath(f"{filename}"), "a", encoding="utf-8") as f:
+                        f.write(
+                            f"\n [{formatted_time_error_start}] {func.__name__} started with inputs: "
+                            f"{args}, {kwargs}"
+                            f"\n [{formatted_time_error_end}] {func.__name__}"
+                            f"error: {type(e).__name__}. Inputs: {args}, {kwargs}"
+                        )
                 else:
-                    print(f"{func.__name__} error: {type(e).__name__}. Inputs: {args}, {kwargs}")
-                raise   TypeError(f"Введены не правильные данные в функции {func.__name__}")
+                    print(
+                        f" \n [{formatted_time_error_start}] {func.__name__} started with inputs:"
+                        f"{args}, {kwargs}\n [{formatted_time_error_end}] {func.__name__}"
+                        f"error: {type(e).__name__}. Inputs: {args}, {kwargs}"
+                    )
+                raise TypeError(f"Введены не правильные данные в функции {func.__name__}")
+
         return wrapper
+
     return decorator
 
 
-@log(filename="my_log.txt")
+@log()
 def my_function(x, y):
     """Сложение чисел"""
     return x + y
 
-my_function(1.5, 2)
-# print(my_function(1, 2))
 
+my_function(1, 2)
+# print(my_function(1, 2))
---
+++ tests/test_decorators.py
@@ -0,0 +1,21 @@
+import pytest
+from freezegun import freeze_time
+
+from src.decorators import my_function, log
+
+@pytest.mark.parametrize("x, y, expected_output", [
+    (1, 2, "\n[2025-03-28 12:24:25] my_function started with inputs: (1, 2), {}\n"
+            "[2025-03-28 12:24:25] my_function finished successfully with result: 3")
+])
+@freeze_time("2025-03-28 12:24:25")  # Замораживаем время на нужной дате и времени
+
+def test_log(capsys, x, y, expected_output):
+    @log()
+    def my_function(x, y):
+        return x + y
+
+    my_function(x, y)  # вызываем функцию, чтобы сработал декоратор
+    captured = capsys.readouterr()
+    assert captured.out.strip() == expected_output.strip()
+
+
