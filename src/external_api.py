f61bdc95996651c6f71d2dfdd932b60d45b71610
DGBIZT <georgiy.gorlov@gmail.com>
2025-04-05 23:27:54 +0300

    Написал две функции модулях utils и external_api, создал файлы .env и .env.example

---
+++ src/external_api.py
@@ -0,0 +1,34 @@
+import requests
+import json
+from dotenv import load_dotenv
+import os
+
+
+def transaction_amount(transaction_dict: dict) -> float:
+    """Функция принимает на вход транзакцию и возвращает сумму транзакции в рублях"""
+
+    amount = transaction_dict["operationAmount"]["amount"]
+    currency = transaction_dict["operationAmount"]["currency"]["code"]
+    to_forex = "RUB"
+
+    if currency == "RUB":
+
+        return round(float(amount), 2)
+
+    else:
+        load_dotenv()
+        api_key = os.getenv("API_KEY")
+
+        url = f"https://api.apilayer.com/exchangerates_data/convert?to={to_forex}&from={currency}&amount={amount}"
+        payload = {}
+        headers = {"apikey": f"{api_key}"}
+        try:
+            response = requests.request("GET", url, headers=headers, data=payload)
+
+        except requests.exceptions.RequestException:
+            print("ошибка http запроса")
+            return 0
+        else:
+            result = response.text
+            json_data = json.loads(result)
+            return round(json_data["result"], 2)
---
+++ src/utils.py
@@ -0,0 +1,18 @@
+import json
+
+
+def get_for_city(file_path: str) -> list:
+    """Принимает на вход путь до JSON-файла и возвращает список словарей с данными о финансовых транзакциях"""
+    try:
+        with open(file_path, "r", encoding="UTF-8") as f:
+            try:
+                data = json.load(f)
+                return data
+            except json.JSONDecodeError:
+                return list()
+    except FileNotFoundError:
+        return list()
+
+
+a = get_for_city("data/operations.json")
+print(a)
