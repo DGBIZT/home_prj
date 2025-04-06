ecdc8a113b9de86ea45d05db269eb5fcfedf51c6
DGBIZT <georgiy.gorlov@gmail.com>
2025-04-06 10:21:14 +0300

    Добавил пару тестов к utils

--- src/utils.py
+++ src/utils.py
@@ -1,18 +1,27 @@
 import json
+import os
+
 
 
 def get_for_city(file_path: str) -> list:
     """Принимает на вход путь до JSON-файла и возвращает список словарей с данными о финансовых транзакциях"""
+    base_dir = os.path.dirname(__file__)
+    full_path = os.path.join(base_dir, file_path)
     try:
-        with open(file_path, "r", encoding="UTF-8") as f:
+        with open(full_path, "r", encoding="UTF-8") as f:
             try:
                 data = json.load(f)
+                if type(data) is not list:
+                    return []
                 return data
+            # Ошибка обработки, декодирования файла
             except json.JSONDecodeError:
                 return list()
+            # Файл не найден
     except FileNotFoundError:
         return list()
 
 
-# a = get_for_city("data/operations.json")
-# print(a)
+a = get_for_city("../data/operations.json")
+print(a)
+
---
+++ tests/test_utils.py
@@ -0,0 +1,52 @@
+
+from src.utils import get_for_city
+from unittest.mock import patch
+from io import StringIO
+
+
+
+@patch('src.utils.get_for_city')  # Убедись, что путь указан правильно
+def test_get_for_city(mock_get_for_city):
+    expected_data = [{'id': 441945886, 'state': 'EXECUTED', 'date': '2019-08-26T10:50:58.294041', 'operationAmount': {'amount': '31957.58', 'currency': {'name': 'руб.', 'code': 'RUB'}}, 'description': 'Перевод организации', 'from': 'Maestro 1596837868705199', 'to': 'Счет 64686473678894779589'}, {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364', 'operationAmount': {'amount': '8221.37', 'currency': {'name': 'USD', 'code': 'USD'}}, 'description': 'Перевод организации', 'from': 'MasterCard 7158300734726758', 'to': 'Счет 35383033474447895560'}]
+    mock_get_for_city.return_value = expected_data
+    # result = get_for_city("../data/operations.json")
+    # print(f"Результат вызова get_for_city: {result}")
+    assert mock_get_for_city("../data/operations.json") == expected_data
+    mock_get_for_city.assert_called_once_with("../data/operations.json")
+
+
+def test_get_for_city_not_found():
+    with patch('builtins.open', side_effect=FileNotFoundError):
+        result = get_for_city('non_existent_file.txt')
+        assert result == []
+
+def test_get_for_city_with_invalid_data():
+    invalid_json_data = "{invalid_json}"
+    result = get_for_city(invalid_json_data)
+    assert result == []
+
+
+# def load_json(data):
+#     try:
+#         return json.loads(data)
+#     except json.JSONDecodeError:
+#         return list()
+#
+# # Тестовая функция
+# def test_load_json_with_invalid_data():
+#     invalid_json_data = "{invalid_json}"
+#     result = load_json(invalid_json_data)
+#     assert result == []
+
+def test_get_for_city_type_not_list():
+    fake_file = StringIO('{"key": "value"}')
+    content = fake_file.getvalue()
+    print(f"Результат вызова get_for_city: {content}")
+    result = get_for_city(content)
+    assert result == []
+
+def test_get_for_city_type_list():
+    fake_file = StringIO('[1, 2, 3]')
+    content = fake_file.getvalue()
+    result = get_for_city(content)
+    assert result == [1, 2, 3]
